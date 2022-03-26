c4bf6b3011ff9ac51b3fe3ae7f8ca2235d4cd937
Akio Tanaka <akimaron2828@icloud.com>
2022-03-26 22:27:44 +0900

    refactor: 変数名変更、useTimerの依存を排除

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,77 +1,31 @@
 import { useCallback, useEffect, useState } from 'react'
-import { wordList } from 'dev/wordList'
 
 interface wordData {
   value: string
   enemy: boolean
 }
 
-export const useTimer = () => {
+export const useTimer = (endSecond: number) => {
   const [count, setCount] = useState<number>(0)
   const [timer, setTimer] = useState<boolean>(false)
-  const [words, setWords] = useState<wordData[]>([
-    { value: 'start', enemy: false },
-  ])
-  const [words2, setWords2] = useState<wordData[]>([
-    { value: 'start', enemy: false },
-  ])
-
-  // 単語をランダムに
-  const shuffleArray = useCallback((array: string[]) => {
-    const cloneArray = [...array]
-
-    for (let i = cloneArray.length - 1; i >= 0; i--) {
-      let rand = Math.floor(Math.random() * (i + 1))
-      let tmpStorage = cloneArray[i]
-      cloneArray[i] = cloneArray[rand]
-      cloneArray[rand] = tmpStorage
-    }
-
-    return cloneArray
-  }, [])
-
-  const shuffleWords = shuffleArray(wordList)
-  const shuffleWords2 = shuffleArray(wordList)
 
   const countup = useCallback(() => {
     setCount((count) => count + 1)
   }, [])
 
-  const addTodo = useCallback(
-    (word: string, enemy: boolean) => {
-      const newWords = [...words, { value: word, enemy: enemy }]
-      setWords(newWords)
-    },
-    [words]
-  )
-  const addTodo2 = useCallback(
-    (word: string, enemy: boolean) => {
-      const newWords = [...words2, { value: word, enemy: enemy }]
-      setWords2(newWords)
-    },
-    [words2]
-  )
-
   useEffect(() => {
-    // 2秒毎にwordsに単語を追加
-    if (count != 0 && count % 2 == 0) {
-      addTodo(shuffleWords[count / 2], false)
-      addTodo2(shuffleWords2[count / 2], false)
+    if (count - 1 > endSecond) {
+      setTimer(false)
     }
-    if (count > 59) setTimer(false)
     if (timer) {
       const timerId = setInterval(countup, 1000)
       return () => clearInterval(timerId)
     }
-  }, [timer, count])
+  }, [timer, count, countup, endSecond])
 
   return {
     timer,
     setTimer,
     count,
-    words,
-    words2,
-    addTodo,
-    addTodo2,
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,72 @@
 import Link from 'next/link'
-import React, { useCallback, useEffect, useState, VFC } from 'react'
+import React, { useCallback, useEffect, useMemo, useState, VFC } from 'react'
+import { wordList } from 'dev/wordList'
 import { useTimer } from 'hooks/useTimer'
+
+const END_SECOND = 60
+
+interface wordData {
+  value: string
+  enemy: boolean
+}
 
 const Home: VFC = () => {
   const [text, setText] = useState<string>('')
-  const [text2, setText2] = useState<string>('')
+  const [enemyText, setEnemyText] = useState<string>('')
   const [index, setIndex] = useState<number>(0)
-  const [index2, setIndex2] = useState<number>(0)
-  const { timer, setTimer, count, words, words2, addTodo, addTodo2 } =
-    useTimer()
+  const [enemyIndex, setEnemyIndex] = useState<number>(0)
+  const [words, setWords] = useState<wordData[]>([
+    { value: 'start', enemy: false },
+  ])
+  const [enemyWords, setEnemyWords] = useState<wordData[]>([
+    { value: 'start', enemy: false },
+  ])
+  const { timer, setTimer, count } = useTimer(END_SECOND)
 
+  // 追加する単語をランダムに
+  const shuffleArray = useCallback((array: string[]) => {
+    const cloneArray = [...array]
+
+    for (let i = cloneArray.length - 1; i >= 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1))
+      let tmpStorage = cloneArray[i]
+      cloneArray[i] = cloneArray[rand]
+      cloneArray[rand] = tmpStorage
+    }
+
+    return cloneArray
+  }, [])
+
+  const shuffleWords = useMemo(() => shuffleArray(wordList), [shuffleArray])
+  const shuffleEnemyWords = useMemo(
+    () => shuffleArray(wordList),
+    [shuffleArray]
+  )
+
+  const addWord = useCallback(
+    (word: string, enemy: boolean): void => {
+      const newWords = [...words, { value: word, enemy: enemy }]
+      setWords(newWords)
+    },
+    [words]
+  )
+  const addEnemyWord = useCallback(
+    (word: string, enemy: boolean): void => {
+      const newWords = [...enemyWords, { value: word, enemy: enemy }]
+      setEnemyWords(newWords)
+    },
+    [enemyWords]
+  )
+
+  // 単語を2秒ごとに追加
+  useEffect(() => {
+    if (count != 0 && count % 2 == 0) {
+      addWord(shuffleWords[count / 2], false)
+      addEnemyWord(shuffleEnemyWords[count / 2], false)
+    }
+  }, [count, shuffleWords, shuffleEnemyWords])
+
+  // 入力する度実行
   const changeText = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       if (!timer) setTimer(true)
@@ -18,10 +75,10 @@ const Home: VFC = () => {
     [timer, setTimer]
   )
 
-  const ChangeText2 = useCallback(
+  const ChangeEnemyText = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       if (!timer) setTimer(true)
-      setText2(e.target.value)
+      setEnemyText(e.target.value)
     },
     [timer, setTimer]
   )
@@ -31,21 +88,30 @@ const Home: VFC = () => {
       setText('')
       // 相手から送られた単語出ないなら、相手に送る
       if (words[index].enemy == false) {
-        addTodo2(words[index].value, true)
+        addEnemyWord(words[index].value, true)
       }
       words[index] = words[index + 1]
       setIndex(index + 1)
     }
 
-    if (words2[index2] && text2 === words2[index2].value) {
-      setText2('')
-      if (words2[index2].enemy == false) {
-        addTodo(words2[index2].value, true)
+    if (enemyWords[enemyIndex] && enemyText === enemyWords[enemyIndex].value) {
+      setEnemyText('')
+      if (enemyWords[enemyIndex].enemy == false) {
+        addWord(enemyWords[enemyIndex].value, true)
       }
-      words2[index2] = words2[index2 + 1]
-      setIndex2(index2 + 1)
+      enemyWords[enemyIndex] = enemyWords[enemyIndex + 1]
+      setEnemyIndex(enemyIndex + 1)
     }
-  }, [text, text2])
+  }, [
+    text,
+    enemyText,
+    index,
+    enemyIndex,
+    words,
+    enemyWords,
+    addWord,
+    addEnemyWord,
+  ])
 
   return (
     <div className='mx-auto mt-4 w-[90%] max-w-[800px]'>
@@ -95,13 +161,16 @@ const Home: VFC = () => {
                     autoFocus
                     value={text}
                     onChange={changeText}
-                    disabled={count > 59}
+                    disabled={count > END_SECOND - 1}
                   />
                 </div>
               </div>
               <div className='font-serif text-[50px] text-center'>
                 <div>{index}</div>
-                <div>{count > 59 && (index > index2 ? 'Win' : 'Lose')}</div>
+                <div>
+                  {count > END_SECOND - 1 &&
+                    (index > enemyIndex ? 'Win' : 'Lose')}
+                </div>
               </div>
             </div>
 
@@ -109,11 +178,12 @@ const Home: VFC = () => {
               <div className='p-4 mx-auto mt-10 w-[300px] text-lg text-center rounded-md border-2 border-black shadow-xl'>
                 <ul className='flex flex-col-reverse h-[250px]'>
                   <li className='text-2xl'>
-                    {words2[index2 + 1] && words2[index2 + 1].value}
+                    {enemyWords[enemyIndex + 1] &&
+                      enemyWords[enemyIndex + 1].value}
                   </li>
-                  {words2.map((word, i) => (
+                  {enemyWords.map((word, i) => (
                     <li key={i}>
-                      {index2 + 1 < i && i < index2 + 9 && word.value}
+                      {enemyIndex + 1 < i && i < enemyIndex + 9 && word.value}
                     </li>
                   ))}
                 </ul>
@@ -121,14 +191,14 @@ const Home: VFC = () => {
                 {/* 入力する単語 */}
                 <div className='text-center'>
                   <div className='h-[40px] text-3xl'>
-                    {words2[index2] &&
-                      words2[index2].value.split('').map((v, i) => (
+                    {enemyWords[enemyIndex] &&
+                      enemyWords[enemyIndex].value.split('').map((v, i) => (
                         <span
                           key={i}
                           className={`${
-                            text2.length > i &&
-                            words2[index2].value.split('')[i] !=
-                              text2.split('')[i] &&
+                            enemyText.length > i &&
+                            enemyWords[enemyIndex].value.split('')[i] !=
+                              enemyText.split('')[i] &&
                             'text-red-500'
                           }`}
                         >
@@ -140,15 +210,18 @@ const Home: VFC = () => {
                     type='text'
                     className='my-2 text-xl border-2 border-black'
                     autoFocus
-                    value={text2}
-                    onChange={ChangeText2}
-                    disabled={count > 59}
+                    value={enemyText}
+                    onChange={ChangeEnemyText}
+                    disabled={count > END_SECOND - 1}
                   />
                 </div>
               </div>
               <div className='font-serif text-[50px] text-center'>
-                <div>{index2}</div>
-                <div>{count > 59 && (index2 > index ? 'Win' : 'Lose')}</div>
+                <div>{enemyIndex}</div>
+                <div>
+                  {count > END_SECOND - 1 &&
+                    (enemyIndex > index ? 'Win' : 'Lose')}
+                </div>
               </div>
             </div>
           </div>
