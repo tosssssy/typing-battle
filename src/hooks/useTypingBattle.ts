b38d219f18e28ac01a77db26874a672024fd285b
t-kabu <t.kabu.03@gmail.com>
2022-03-28 14:04:15 +0900

    feat: firestoreからwordを受け取って処理するhooks作成

---
+++ src/hooks/useTypingBattle.ts
@@ -0,0 +1,110 @@
+import {
+  getDocs,
+  query,
+  orderBy,
+  onSnapshot,
+  CollectionReference,
+  DocumentData,
+} from 'firebase/firestore'
+import { useCallback, useEffect, useState } from 'react'
+import { Word } from 'types/word'
+
+export const useTypingBattle = (
+  reference: CollectionReference<DocumentData>,
+  userName: string
+) => {
+  const [enemyName, setEnemyName] = useState<string>('')
+  const [allWords, setAllWords] = useState<Word[]>([])
+  const [displayWords, setDisplayWords] = useState<Word[]>([])
+  const [displayEnemyWords, setDisplayEnemyWords] = useState<Word[]>([])
+
+  // firestoreからのデータ取得（初回）
+  const getAllWordsFromFirestore = useCallback(async () => {
+    const querySnapshot = await getDocs(reference)
+    // 敵の名前を登録
+    if (!enemyName) {
+      let isRegisteredEnemyName = false
+      querySnapshot.forEach((doc) => {
+        if (!isRegisteredEnemyName && doc.data().userName !== userName) {
+          isRegisteredEnemyName = true
+          setEnemyName(doc.data().userName)
+          console.log('enemy name: ', doc.data().userName)
+        }
+      })
+    }
+
+    // 振り分ける処理
+    let displayWords: Word[] = []
+    let displayEnemyWords: Word[] = []
+    querySnapshot.forEach((doc) => {
+      const newWord = doc.data() as Word
+      console.log(newWord.userName, '->', newWord.value)
+      if (isOwnerDisplayWord(userName, newWord)) {
+        displayWords = [...displayWords, newWord]
+      }
+      if (isOwnerDisplayWord(enemyName, newWord)) {
+        displayEnemyWords = [...displayEnemyWords, newWord]
+      }
+    })
+    setAllWords(querySnapshot.docs.map((doc) => doc.data()))
+    setDisplayWords(displayWords)
+    setDisplayEnemyWords(displayEnemyWords)
+  }, [enemyName, reference, userName])
+  useEffect(() => {
+    getAllWordsFromFirestore()
+  }, [getAllWordsFromFirestore])
+
+  // firestoreからのデータ取得（購読）
+  useEffect(() => {
+    const q = query(reference, orderBy('createdAt', 'asc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === 'modified') {
+          const newWord = change.doc.data() as Word
+
+          // なぜか同じものが2回取得されるため
+          if (newWord.value === allWords[allWords.length - 1].value) {
+            return
+          }
+          // 敵の名前を登録（動作未確認）
+          if (!enemyName && newWord.userName && newWord.userName !== userName) {
+            setEnemyName(newWord.userName)
+            console.log('enemy name: ', newWord.userName)
+          }
+
+          // 振り分け処理
+          setAllWords([...allWords, newWord])
+          if (isOwnerDisplayWord(userName, newWord)) {
+            setDisplayWords([...displayWords, newWord])
+          }
+          if (isOwnerDisplayWord(enemyName, newWord)) {
+            setDisplayEnemyWords([...displayEnemyWords, newWord])
+          }
+        }
+      })
+    })
+    return unsubscribe
+  }, [
+    allWords,
+    displayEnemyWords,
+    displayWords,
+    enemyName,
+    reference,
+    userName,
+  ])
+
+  return { enemyName, displayWords, displayEnemyWords }
+}
+
+// owner側に表示されるコメントかを判定。以下の2通りある。
+// 1. 敵から送られてきた単語
+// 2. 敵のbotから送られてきた単語
+const isOwnerDisplayWord = (ownerName: string, word: Word) => {
+  if (word.type === 'obstacle' && word.userName !== ownerName) {
+    return true
+  }
+  if (word.type === 'bot' && word.userName !== ownerName) {
+    return true
+  }
+  return false
+}
