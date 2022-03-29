b3540c990232bb14af492d4d012656a95f463c0f
t-kabu <t.kabu.03@gmail.com>
2022-03-29 10:11:44 +0900

    fix: botで自分側に単語を送るように変更

--- src/hooks/useWordSendingBot.ts
+++ src/hooks/useWordSendingBot.ts
@@ -8,10 +8,10 @@ import { useCallback, useEffect } from 'react'
 import { useTimer } from './useTimer'
 import { Word } from 'types/word'
 
-//一定間隔に一度敵に単語を送るbot
+//一定間隔に一度自分に単語を送るbot
 export const useWordSendingBot = (
   reference: CollectionReference<DocumentData>,
-  userName: string,
+  enemyName: string,
   playingTime: number
 ) => {
   const { count, startTimer } = useTimer(playingTime)
@@ -19,7 +19,7 @@ export const useWordSendingBot = (
   const sendRandomWord = useCallback(async () => {
     try {
       const word: Word = {
-        userName: userName,
+        userName: enemyName,
         value: String(count),
         createdAt: new Date(),
         type: 'bot',
@@ -29,13 +29,13 @@ export const useWordSendingBot = (
     } catch (e) {
       console.error('Error adding document: ', e)
     }
-  }, [count, reference, userName])
+  }, [count, reference, enemyName])
 
   useEffect(() => {
     if (count > 0 && count < playingTime && count % 3 === 0) {
       sendRandomWord()
     }
-  }, [count, sendRandomWord, playingTime, userName])
+  }, [count, sendRandomWord, playingTime, enemyName])
 
   return {
     botStart: startTimer,
